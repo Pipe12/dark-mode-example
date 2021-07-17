8bfc34660fc9f7047ee70e23d61b9859776eecac
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2021-07-16 19:26:41 -0500

    Create themes

---
+++ src/utils/themes.js
@@ -0,0 +1,15 @@
+export const lightTheme = {
+  backgroundBody: '--light-mode-background',
+  text: '--light-mode-text',
+  backgroundElement: '--light-mode-element',
+  input: '--light-mode-input',
+  shadow: '--light-mode-shadow'
+}
+
+export const darkTheme = {
+  backgroundBody: '--dark-mode-background',
+  text: '--dark-mode-text',
+  backgroundElement: '--dark-mode-element',
+  input: '--dark-mode-input',
+  shadow: '--dark-mode-shadow'
+}
